7d1a431083536ff7d88feff2399f4f9b0900502b
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-04 19:04:40 +0530

    Basic setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import info from "./data.json";
+import LeftComp from "./components/LeftComp";
+import RightComp from "./components/RightComp";
+
+type Data = {
+  category: string;
+  score: number;
+  icon: string;
+}[];
+
+const data = info as Data;
+
+function App() {
+  return (
+    <div className="main">
+      <LeftComp scoresInfo={data} />
+      <RightComp scoresInfo={data} />
+    </div>
+  );
+}
+
+export default App;
